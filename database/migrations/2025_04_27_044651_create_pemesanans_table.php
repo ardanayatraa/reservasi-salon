e25b2809dda857b278fa6099158d8f0c43001043
Ardana Yatra <imadeardanayatra@gmail.com>
2025-05-27 01:53:32 +0800

    fix all finish

--- database/migrations/2025_04_27_044651_create_pemesanans_table.php
+++ database/migrations/2025_04_27_044651_create_pemesanans_table.php
@@ -29,8 +29,7 @@ class CreatePemesanansTable extends Migration
             $table->foreign('id_pelanggan')
                   ->references('id_pelanggan')->on('pelanggans')
                   ->onDelete('cascade');
-            $table->foreign('id_perawatan')
-                  ;
+
         });
     }
 
