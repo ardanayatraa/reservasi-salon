2ac4582d8e1fd592dcc8efb202723ce9326865be
Ardana Yatra <imadeardanayatra@gmail.com>
2025-05-27 01:55:34 +0800

    fix all finish

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(AccountSeeder::class);
-        $this->call(PerawatanSeeder::class);
+        // $this->call(PerawatanSeeder::class);
     }
 }
