af72715adc496d1260c0ae0d545c254f6092aeee
Ardana Yatra <imadeardanayatra@gmail.com>
2025-06-25 17:20:16 +0800

    fix seed

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -19,24 +19,5 @@ class AccountSeeder extends Seeder
             'no_telepon' => '081234567890',
             'alamat'     => 'Jl. Merdeka No.1, Jakarta',
         ]);
-
-        // Akun Pelanggan
-        Pelanggan::create([
-            'nama_lengkap' => 'Budi Santoso',
-            'username'     => 'pelanggan',
-            'password'     => Hash::make('password'),
-            'email'        => 'budi@example.com',
-            'no_telepon'   => '082112345678',
-            'alamat'       => 'Jl. Melati No.5, Bandung',
-        ]);
-
-        Pelanggan::create([
-            'nama_lengkap' => 'Siti Aminah',
-            'username'     => 'siti',
-            'password'     => Hash::make('rahasia456'),
-            'email'        => 'siti@example.com',
-            'no_telepon'   => '085212345678',
-            'alamat'       => 'Jl. Anggrek No.10, Surabaya',
-        ]);
     }
 }
