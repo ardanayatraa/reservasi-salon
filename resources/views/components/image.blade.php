ca59013db1bd5b90b20371a847db2d828baab217
Ardana Yatra <imadeardanayatra@gmail.com>
2025-07-09 09:07:37 +0800

    tampilan

--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -1,5 +1,5 @@
 @if ($foto)
-    <img src="{{ asset('storage/perawatan/' . $foto) }}" alt="Foto Perawatan" class="h-16 w-16 object-cover rounded-md">
+    <img src="{{ asset('storage/' . $foto) }}" alt="Foto Perawatan" class="h-16 w-16 object-cover rounded-md">
 @else
     <span class="text-gray-400 italic">Tidak ada foto</span>
 @endif
